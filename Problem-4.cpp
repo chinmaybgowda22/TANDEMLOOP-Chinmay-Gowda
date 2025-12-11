6503f355b41d18ad0726c7fb8ae213481ccef928
chinmaybgowda22 <chinmaybgowda789@gmail.com>
2025-12-11 11:19:57 +0530

    Implement divisibility count for numbers 1 to 9
    This program counts the number of elements in 'nums' that are divisible by each integer from 1 to 9 and stores the results in a map.

---
+++ Problem-4.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <map>
+using namespace std;
+
+int main() {
+    vector<int> nums = {1,2,8,9,12,46,76,82,15,20,30};
+    map<int,int> result;
+
+    for (int i = 1; i <= 9; i++) {
+        int count = 0;
+        for (int n : nums)
+            if (n % i == 0) count++;
+        result[i] = count;
+    }
+
+    for (auto &p : result)
+        cout << p.first << ": " << p.second << endl;
+
+    return 0;
+}
